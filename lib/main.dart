26d1bad8cf7a9d63faed5e0ad53ff8e0b81bcd39
Aaron Murphy <aamurphy98@gmail.com>
2024-06-17 13:22:29 -0400

    Implemented Xylophone App
    Created UI using a build widget method to create xylophone keys. Utilized audioplayer package to play audio assets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(const XylophoneApp());
 }
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  void playSound(int noteNumber) {
+    var player = AudioPlayer();
+    player.play(AssetSource('note$noteNumber.wav'));
+  }
+
+  Widget buildKey(Color color, String label, int note) {
+    return
+      Expanded(
+        child: MaterialButton(
+          color: color,
+          onPressed: () async {
+            playSound(note);
+          },
+          child: Center(
+            child: Text(
+              label,
+              style: const TextStyle(
+                fontSize: 30.0
+              ),
+            ),
+          ),
+        ),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 'C', 1),
+              buildKey(Colors.orange, 'D', 2),
+              buildKey(Colors.yellow, 'E', 3),
+              buildKey(Colors.green, 'F', 4),
+              buildKey(Colors.blue, 'G', 5),
+              buildKey(Colors.indigo, 'A', 6),
+              buildKey(Colors.purple, 'B', 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
